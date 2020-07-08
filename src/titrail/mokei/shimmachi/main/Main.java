1ba754a869003c7c4d485a2fc9ede7f12529f2c2
Syata Kurasi <soccer_Rs0515@yahoo.co.jp>
2020-07-08 16:39:44 +0900

    Added addTrack method in RailroadWiring class.

--- src/titrail/mokei/shimmachi/main/Main.java
+++ src/titrail/mokei/shimmachi/main/Main.java
@@ -1,10 +1,29 @@
 package titrail.mokei.shimmachi.main;
 
+import titrail.mokei.shimmachi.commands.*;
 import titrail.mokei.shimmachi.commands.cui.*;
+import titrail.mokei.shimmachi.railroad.*;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class Main {
+	static final String LOG_FILENAME = "./log/";
 	public static void main (String[] args) {
+		try {
+			Date date = new Date ();
+			DateFormat dFormat = new SimpleDateFormat ("yyyyMMdd-HHmmss");
+			String logFilename = LOG_FILENAME + dFormat.format (date);
 
+			Track t1 = new TrackLogger (logFilename);
+			Track t2 = new TrackLogger (logFilename);
+			RailroadWiring wiring = new RailroadWiringLogger (logFilename);
+			wiring.addTrack (t1);
+			wiring.addTrack (t2);
+		} catch (Exception e) {
+			e.printStackTrace ();
+		}
 	}
 }
 
